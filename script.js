adc26aa7298e9f90d511fe25a4afe1c3b8812b94
Deepak Acharya K <129195970+DeepakAcharyaK@users.noreply.github.com>
2024-04-20 00:23:33 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// get the data
 let userdata=[{
     profilepic:"https://images.unsplash.com/photo-1586907835000-f692bbd4c9e0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGdpcmxzfGVufDB8fDB8fHww",
     pendmsg:4,
@@ -6,10 +5,22 @@ let userdata=[{
     name:'Harshitha',
     age:53,
     intrest:[
-        "Music",
-        "Movies",
-        "Sports",
-        "Travel"
+        {
+            hicon:'<i class="ri-edit-2-fill"></i>',
+            hname:'Writing'
+        },
+        {
+            hicon:`<i class="ri-music-2-fill"></i>`,
+            hname:'Music'
+        },
+        {
+            hicon:`<i class="ri-football-fill"></i>`,
+            hname:'Sports'
+        },
+        {
+            hicon:`<i class="ri-run-line"></i>`,
+            hname:'Running'
+        }
     ],
     bio:"I am a music enthusiast and I like to listen to music. I also like to play video games and watch movies. I also like to play sports. I also like to travel.",
     isfriend:null
@@ -21,10 +32,22 @@ let userdata=[{
     name:'Deepa',
     age:26,
     intrest:[
-        "Music",
-        "Movies",
-        "Sports",
-        "Travel"
+        {
+            hicon:'<i class="ri-edit-2-fill"></i>',
+            hname:'Writing'
+        },
+        {
+            hicon:`<i class="ri-music-2-fill"></i>`,
+            hname:'Music'
+        },
+        {
+            hicon:`<i class="ri-football-fill"></i>`,
+            hname:'Sports'
+        },
+        {
+            hicon:`<i class="ri-run-line"></i>`,
+            hname:'Running'
+        }
     ],
     bio:"I am a music enthusiast and I like to listen to music. I also like to play video games and watch movies. I also like to play sports. I also like to travel.",
     isfriend:null
@@ -36,10 +59,22 @@ let userdata=[{
     name:'Anusha',
     age:53,
     intrest:[
-        "Music",
-        "Movies",
-        "Sports",
-        "Travel"
+        {
+            hicon:'<i class="ri-edit-2-fill"></i>',
+            hname:'Writing'
+        },
+        {
+            hicon:`<i class="ri-music-2-fill"></i>`,
+            hname:'Music'
+        },
+        {
+            hicon:`<i class="ri-football-fill"></i>`,
+            hname:'Sports'
+        },
+        {
+            hicon:`<i class="ri-run-line"></i>`,
+            hname:'Running'
+        }
     ],
     bio:"I am a music enthusiast and I like to listen to music. I also like to play video games and watch movies. I also like to play sports. I also like to travel.",
     isfriend:null
@@ -51,10 +86,22 @@ let userdata=[{
     name:'Vishu',
     age:13,
     intrest:[
-        "Music",
-        "Movies",
-        "Sports",
-        "Travel"
+        {
+            hicon:'<i class="ri-edit-2-fill"></i>',
+            hname:'Writing'
+        },
+        {
+            hicon:`<i class="ri-music-2-fill"></i>`,
+            hname:'Music'
+        },
+        {
+            hicon:`<i class="ri-football-fill"></i>`,
+            hname:'Sports'
+        },
+        {
+            hicon:`<i class="ri-run-line"></i>`,
+            hname:'Running'
+        }
     ],
     bio:"I am a music enthusiast and I like to listen to music. I also like to play video games and watch movies. I also like to play sports. I also like to travel.",
     isfriend:null
@@ -73,6 +120,22 @@ function select(element) {
    select('.profileimg img').src=userdata[curr].profilepic;
    select('.badge h5').innerHTML=userdata[curr].pendmsg;
    select('.heading h1').textContent=userdata[curr].location;
+   select('.bio').textContent=userdata[curr].bio;
+   select('.name h1:nth-child(1)').textContent=userdata[curr].name;
+   select('.name h1:nth-child(2)').textContent=userdata[curr].age;
+
+   let clutter="";
+   userdata[curr].intrest.forEach((value)=>{
+   let {hicon,hname}=value;
+
+        clutter+=`<div class="tag flex items-center bg-white/40 px-5 py-2 rounded-full gap-4">
+        ${hicon} 
+        <h3>
+        ${hname}
+        </h3>
+      </div>`
+   })  
+   select('.tags').innerHTML=clutter;
 
 
 })();
